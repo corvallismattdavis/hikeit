ee107bd9b88277e05240b5a0ffebcd68b97ce84d
Jon Brausch <jbrausch@calpoly.edu>
2017-04-09 16:35:03 -0700

    Moved hike activity to its own place. Started on search activity.

---
+++ HikeIt/app/src/main/java/com/hikeit/HikeActivity.java
@@ -0,0 +1,20 @@
+package com.hikeit;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HikeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hike);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
