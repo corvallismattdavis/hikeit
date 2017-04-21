174afd945bb114019da980cc3b5c54480546f68f
jbrausch <jbrausch@calpoly.edu>
2017-04-20 23:33:12 -0700

    Search list is now clickable and linked to a hike page. Still having trouble loading images from firebase storage on search page though.

--- HikeIt/app/src/main/java/com/hikeit/DisplaySearchActivity.java
+++ HikeIt/app/src/main/java/com/hikeit/DisplaySearchActivity.java
@@ -1,26 +1,40 @@
 package com.hikeit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
 
 public class DisplaySearchActivity extends AppCompatActivity {
 
-    ArrayList<HikeListItem> arrayList= new ArrayList<HikeListItem>();
-    ListAdapter adapter;
-    Context curContext;
+    private FirebaseDatabase fbDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference rootRef = fbDatabase.getReference();
+    private DatabaseReference childHikeRef = rootRef.child("hikes");
+
+    private ArrayList<HikeListItem> allHikes = new ArrayList<HikeListItem>();
+    private ListAdapter adapter;
+    private Context curContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +42,55 @@ public class DisplaySearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.d("ON CREATE", "created activity");
         curContext = this;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        allHikes = new ArrayList<HikeListItem>();
+
         final ListView hikeList = (ListView) findViewById(R.id.hike_list);
 
-        arrayList.add(new HikeListItem("bishops", "Bishop Peak", HikeListItem.Difficulty.Easy, 4.5f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "Madonna", HikeListItem.Difficulty.Moderate, 4.8f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "Avila Ridge", HikeListItem.Difficulty.Hard, 4.2f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "Cabrillo", HikeListItem.Difficulty.Vigorous, 3.7f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "Cal Poly \"P\"", HikeListItem.Difficulty.Easy, 4.5f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "East Cuesta Ridge", HikeListItem.Difficulty.Moderate, 4.8f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "West Cuesta Ridge", HikeListItem.Difficulty.Hard, 4.2f, 3.3f));
-        arrayList.add(new HikeListItem("bishops", "Valencia Peak", HikeListItem.Difficulty.Vigorous, 3.7f, 3.3f));
+        childHikeRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                    HashMap<String, Object> jsonValue = (HashMap<String, Object>)messageSnapshot.getValue();
+                    String title = (String) jsonValue.get("title");
+                    String difficulty = (String) jsonValue.get("difficulty");
+                    String imgSrc = (String) jsonValue.get("imgSrc");
+                    float distance = (float)((double)jsonValue.get("distance"));
+                    float rating = (float)((double)jsonValue.get("rating"));
+
+                    allHikes.add(new HikeListItem(imgSrc, title, HikeListItem.Difficulty.valueOf(difficulty), rating, distance));
+                }
+
+                initAdapter();
+            }
+
+            public void initAdapter()
+            {
+                HikeListAdapter adapter = new HikeListAdapter(curContext, allHikes.toArray(new HikeListItem[0]));
+                hikeList.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+//        allHikes.add(new HikeListItem("bishops", "Bishop Peak", HikeListItem.Difficulty.Easy, 4.5f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "Madonna", HikeListItem.Difficulty.Moderate, 4.8f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "Avila Ridge", HikeListItem.Difficulty.Hard, 4.2f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "Cabrillo", HikeListItem.Difficulty.Vigorous, 3.7f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "Cal Poly \"P\"", HikeListItem.Difficulty.Easy, 4.5f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "East Cuesta Ridge", HikeListItem.Difficulty.Moderate, 4.8f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "West Cuesta Ridge", HikeListItem.Difficulty.Hard, 4.2f, 3.3f));
+//        allHikes.add(new HikeListItem("bishops", "Valencia Peak", HikeListItem.Difficulty.Vigorous, 3.7f, 3.3f));
 
         SearchView searchView = (SearchView) findViewById(R.id.search);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -52,22 +103,24 @@ public class DisplaySearchActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
 //              if (searchView.isExpanded() && TextUtils.isEmpty(newText)) {
-                    //callSearch(newText);
+                //callSearch(newText);
 //              }
                 return true;
             }
 
             public void callSearch(String query) {
-                Collections.shuffle(arrayList);
-                HikeListAdapter adapter = new HikeListAdapter(curContext, arrayList.toArray(new HikeListItem[0]));
+                Collections.shuffle(allHikes);
+                HikeListAdapter adapter = new HikeListAdapter(curContext, allHikes.toArray(new HikeListItem[0]));
                 hikeList.setAdapter(adapter);
             }
 
         });
-
-        HikeListAdapter adapter = new HikeListAdapter(this, arrayList.toArray(new HikeListItem[0]));
-        hikeList.setAdapter(adapter);
     }
 
-
+    public void getHike(View view)
+    {
+        //causes crash
+        Intent startNewHikeActivity = new Intent(this, HikeActivity.class);
+        startActivity(startNewHikeActivity);
+    }
 }
--- HikeIt/app/src/main/java/com/hikeit/HikeListAdapter.java
+++ HikeIt/app/src/main/java/com/hikeit/HikeListAdapter.java
@@ -1,7 +1,14 @@
 package com.hikeit;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.media.Rating;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +16,16 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Jon on 4/12/2017.
@@ -18,6 +35,7 @@ class HikeListAdapter extends BaseAdapter {
     Context context;
     HikeListItem[] data;
     private static LayoutInflater inflater = null;
+    private StorageReference pictureDatabase = FirebaseStorage.getInstance().getReference();
 
     public HikeListAdapter(Context context, HikeListItem[] data) {
         // TODO Auto-generated constructor stub
@@ -46,9 +64,22 @@ class HikeListAdapter extends BaseAdapter {
     }
 
     @Override
+    public boolean areAllItemsEnabled()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(int arg0)
+    {
+        return true;
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
         View vi = convertView;
+
         if (vi == null)
             vi = inflater.inflate(R.layout.list_row, null);
 
@@ -61,11 +92,25 @@ class HikeListAdapter extends BaseAdapter {
         TextView distance = (TextView) vi.findViewById(R.id.list_row_length);
         distance.setText((data[position].distance + " miles"));
 
-        ImageView icon = (ImageView) vi.findViewById(R.id.list_row_img);
-        icon.setImageResource(R.drawable.bishops);
+        final ImageView icon = (ImageView) vi.findViewById(R.id.list_row_img);
+
+        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl("gs://hikeit-c31a4.appspot.com/images/" + data[position].imgSrc + ".jpg");
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d("FILE DOWNLOAD", "Successfully loaded uri");
+                icon.setImageURI(uri);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("FILE DOWNLOAD", "Failed loading uri: " + e.getMessage());
+                icon.setImageResource(R.drawable.empty_photo);
+            }
+        });
 
         RatingBar rating = (RatingBar) vi.findViewById(R.id.list_row_rating);
-        rating.setRating(data[position].rating);
+        rating.setRating(data[position].rating + 10.0f);
         return vi;
     }
 }
